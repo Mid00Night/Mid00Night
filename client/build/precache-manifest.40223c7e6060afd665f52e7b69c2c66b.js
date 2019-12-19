7b687e2ffc1b5a2e1634cbc862daedc25e52bf2b
Mid00Night <hema_fox_hema@yahoo.com>
2019-12-19 16:13:55 +0200

    prepared for deployment

---
+++ client/build/precache-manifest.40223c7e6060afd665f52e7b69c2c66b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e3d3f9bbb7d533ea9e87966c6cd00e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bc164682dbac1b18ce5",
+    "url": "/static/css/main.70165ab1.chunk.css"
+  },
+  {
+    "revision": "acd5e97cc8e3c8c3b310",
+    "url": "/static/js/2.6181fccf.chunk.js"
+  },
+  {
+    "revision": "7bc164682dbac1b18ce5",
+    "url": "/static/js/main.3e799f21.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const connectDB = require('./config/db')
-
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
-
 
 connectDB();
 
+app.use(express.json({ extended: false }));
 
+app.use("/api/products", require("./routes/api/products"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
 
-app.use(express.json({ extended: false }))
-app.get('/', (req,res) => res.send('API Running'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-app.use('/api/products', require('./routes/api/products'))
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
